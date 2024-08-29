8301c30128ac01566222337e96f02d4361afc7fa
Sergio S. Quevedo <sanquev@gmail.com>
2024-08-29 19:35:24 +0200

    Created new order service. It will contain a producer that will send message to the Kafka broker

---
+++ 13_microservice_orders_kafka_producer/src/test/java/org/sergio/products_orders/ProductsOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sergio.products_orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsOrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
