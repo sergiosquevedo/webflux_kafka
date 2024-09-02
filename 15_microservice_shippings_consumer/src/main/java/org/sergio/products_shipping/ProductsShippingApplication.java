7a16b7d4c1cd504bc58f5f882ac3e39953d9a1c9
Sergio S. Quevedo <sanquev@gmail.com>
2024-09-02 11:38:52 +0200

    Created new shipping service in order to create a new Kafka Consumer which will be listening the products' order topic

---
+++ 15_microservice_shippings_consumer/src/main/java/org/sergio/products_shipping/ProductsShippingApplication.java
@@ -0,0 +1,13 @@
+package org.sergio.products_shipping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsShippingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsShippingApplication.class, args);
+	}
+
+}
