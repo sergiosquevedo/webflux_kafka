b1573ac13da8b465e6e18a205835f4b59e795c7d
Sergio S. Quevedo <sanquev@gmail.com>
2024-09-07 13:50:16 +0200

    Created Spring reactive gateway

---
+++ 17_spring_reactive_gateway/src/test/java/org/sergio/reactive_gateway/ReactiveGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sergio.reactive_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
